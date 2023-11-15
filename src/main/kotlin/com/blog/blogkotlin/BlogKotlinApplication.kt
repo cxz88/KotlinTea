76c57724041945393e296f3dc02dfcd568a85ac8
chenxinzhi <c18932379607@163.com>
2023-11-15 17:55:57 +0800

    首次创建后端项目

---
+++ src/main/kotlin/com/blog/blogkotlin/BlogKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.blog.blogkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlogKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<BlogKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/blog/blogkotlin/BlogKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.blog.blogkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
